ae389b8e89ff8b01e74d47513ce0020569e4f142
Micaela Alves <micaela.alves@mercadolivre.com>
2022-03-16 12:06:05 -0300

    feat - exercicio 1

---
+++ modulo_2/aula2/pratica-integradora1/src/exercicio1/Pessoa.java
@@ -0,0 +1,17 @@
+package exercicio1;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+    public String ID;
+    public int peso;
+    public double altura;
+
+    public Pessoa(String nome, int idade, String ID, int peso, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.ID = ID;
+        this.peso = peso;
+        this.altura = altura;
+    }
+}
