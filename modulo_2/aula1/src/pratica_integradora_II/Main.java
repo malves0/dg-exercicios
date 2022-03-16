4a7ad461aa816832e32ecf6a7db17ace582437cd
Micaela Alves <micaela.alves@mercadolivre.com>
2022-03-16 11:35:59 -0300

    pratica

---
+++ modulo_2/aula1/src/pratica_integradora_II/Main.java
@@ -0,0 +1,9 @@
+package pratica_integradora_II;
+
+public class Main {
+}
+
+// categoria recebe participante
+    // exibi o valor a ser pago
+// exibir os participantes de cada categoria
+// remover participante
